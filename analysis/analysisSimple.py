d2263e34d2237b94a2924a553ffad38826262bd9
ece asilar <eceasilar@pc00.idemog.oeaw.ac.at>
2016-02-27 14:01:31 +0100

    add ploting functions to helper, small on analysis python

--- analysis/analysisSimple.py
+++ analysis/analysisSimple.py
@@ -1,20 +1,23 @@
-import ROOT
-from helper import DrawNicePlot
 import os,sys
+import ROOT
+sys.path.append('../python')
+from helper import DrawNicePlot, DrawAdvancedPlot
 
-file = ROOT.TFile('rootfiles/HPQ.root')
+file = ROOT.TFile('../rootfiles/HPQ.root')
 tree = file.Get('LHCO')
 
 ptCut = 15
 etaCut = 2.1
 nPhotonsCut = 2
 selection = 'abs(Photon.Eta)<'+str(etaCut)+'&&Photon.PT>'+str(ptCut)+'&&Photon_size=='+str(nPhotonsCut)
-fileName='PhotonPt_For_selection_'+selection+'_.png'
-path = 'plots/'
+selection_name = '_eta_'+str(etaCut)+'_pt_'+str(ptCut)
+fileName='PhotonPt_For_selection_'+selection_name+'_'
+path = '../plots/'
 if not os.path.exists(path):
   os.makedirs(path)
 
 histo = ROOT.TH1F('histo','Pt',100,0,500)
 tree.Draw('Photon.PT>>histo',selection)
-DrawNicePlot(histo,'Photon Pt',path,fileName)
+#DrawNicePlot(histo,'Photon Pt',path,fileName)
+DrawAdvancedPlot(histo,'Photon P_{T}',path,fileName)
 
